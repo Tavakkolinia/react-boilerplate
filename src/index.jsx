632556e3076b4087912068e657b195148713d440
gergana <gergana_ruse@yahoo.com>
2019-02-02 10:05:09 +0000

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import 'raf/polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+//import App from '../app';
+//create app.jsx file and replace h1 with App
+
+render(<h1>Hello World!</h1>, document.getElementById('root'));
